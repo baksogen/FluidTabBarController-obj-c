76db2686f6c344268c4c3d47a92c2bc433c48ebc
BiOM <biomsoftware@gmail.com>
2020-11-03 12:21:00 +0200

    - initial commit

---
+++ FluidTabBarController/FluidTabBarController-Swift.h
@@ -0,0 +1,13 @@
+//
+//  FluidTabBarController.h
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCFloatingActionButton.
+FOUNDATION_EXPORT double FluidTabBarControllerVersionNumber;
+
+//! Project version string for KCFloatingActionButton.
+FOUNDATION_EXPORT const unsigned char FluidTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluidTabBarController/PublicHeader.h>
---
+++ FluidTabBarController/FluidTabBarController.h
@@ -0,0 +1,16 @@
+//
+//  FluidTabBarController.h
+//
+//  Created by LeeSunhyoup on 2016. 8. 9..
+//  Copyright © 2016년 Lee Sun-Hyoup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCFloatingActionButton.
+FOUNDATION_EXPORT double FluidTabBarControllerVersionNumber;
+
+//! Project version string for KCFloatingActionButton.
+FOUNDATION_EXPORT const unsigned char FluidTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Floaty/PublicHeader.h>
